65c15b75f4cf61ef805000bb251f7c3caf2e2491
hinyeloh <hinyeloh67@gmail.com>
2022-12-07 10:13:36 +0800

    modify profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,7 +20,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request)
     {
-        return view('profile.edit', [
+        return view('profile.editProfile', [
             'user' => $request->user(),
         ]);
     }
